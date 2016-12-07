091dfffcdac4b5f3d64f60164bf4d66d88bb3c6d
electron101 <sabashlive@gmail.com>
2016-12-07 09:17:31 +0300

    Форма ответственных лиц готова. Добавление, удаление, обновление готово.

--- stroimagnat/Form7.Designer.cs
+++ stroimagnat/Form7.Designer.cs
@@ -28,19 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button10 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.textBox_mol_tel = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox_mol_adres = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox_mol_fio = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(645, 281);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(100, 23);
+            this.button10.TabIndex = 19;
+            this.button10.Text = "Удалить";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button11);
+            this.groupBox4.Controls.Add(this.button12);
+            this.groupBox4.Controls.Add(this.textBox_mol_tel);
+            this.groupBox4.Controls.Add(this.label12);
+            this.groupBox4.Controls.Add(this.textBox_mol_adres);
+            this.groupBox4.Controls.Add(this.label13);
+            this.groupBox4.Controls.Add(this.textBox_mol_fio);
+            this.groupBox4.Controls.Add(this.label14);
+            this.groupBox4.Location = new System.Drawing.Point(494, 112);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(263, 163);
+            this.groupBox4.TabIndex = 18;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Добавление/Обновление";
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(151, 126);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(100, 23);
+            this.button11.TabIndex = 8;
+            this.button11.Text = "Обновить";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(151, 97);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(100, 23);
+            this.button12.TabIndex = 7;
+            this.button12.Text = "Добавить";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // textBox_mol_tel
+            // 
+            this.textBox_mol_tel.Location = new System.Drawing.Point(108, 71);
+            this.textBox_mol_tel.Name = "textBox_mol_tel";
+            this.textBox_mol_tel.Size = new System.Drawing.Size(143, 20);
+            this.textBox_mol_tel.TabIndex = 6;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(7, 74);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(52, 13);
+            this.label12.TabIndex = 5;
+            this.label12.Text = "Телефон";
+            // 
+            // textBox_mol_adres
+            // 
+            this.textBox_mol_adres.Location = new System.Drawing.Point(108, 45);
+            this.textBox_mol_adres.Name = "textBox_mol_adres";
+            this.textBox_mol_adres.Size = new System.Drawing.Size(143, 20);
+            this.textBox_mol_adres.TabIndex = 4;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(7, 48);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(38, 13);
+            this.label13.TabIndex = 3;
+            this.label13.Text = "Адрес";
+            // 
+            // textBox_mol_fio
+            // 
+            this.textBox_mol_fio.Location = new System.Drawing.Point(108, 19);
+            this.textBox_mol_fio.Name = "textBox_mol_fio";
+            this.textBox_mol_fio.Size = new System.Drawing.Size(143, 20);
+            this.textBox_mol_fio.TabIndex = 2;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(7, 22);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(37, 13);
+            this.label14.TabIndex = 1;
+            this.label14.Text = "ФИО ";
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.AllowUserToAddRows = false;
+            this.dataGridView4.AllowUserToDeleteRows = false;
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(12, 49);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.ReadOnly = true;
+            this.dataGridView4.Size = new System.Drawing.Size(442, 454);
+            this.dataGridView4.TabIndex = 17;
             // 
             // Form7
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.ClientSize = new System.Drawing.Size(792, 573);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.dataGridView4);
             this.Name = "Form7";
             this.Text = "Ответственные";
+            this.Load += new System.EventHandler(this.Form7_Load);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.TextBox textBox_mol_tel;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox_mol_adres;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox_mol_fio;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.DataGridView dataGridView4;
     }
 }
--- stroimagnat/Form7.cs
+++ stroimagnat/Form7.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace stroimagnat
 {
@@ -15,6 +16,145 @@ namespace stroimagnat
         public Form7()
         {
             InitializeComponent();
+        }
+        void load_mol()                   // функция для отображения информации
+        {
+            Form3.ds.Tables["MOL"].Clear();
+            Form3.strSQL = " SELECT id_mol AS '№_Ответсвенного лица', " +
+                           " name AS 'ФИО', adres AS 'Адрес', tel AS 'Телефон' FROM mol ";
+            Form3.SQLAdapter = new SqlDataAdapter(Form3.strSQL, Form3.cn);
+
+            Form3.SQLAdapter.Fill(Form3.ds, "MOL");
+
+            Form3.bs_mol.DataSource = Form3.ds.Tables["MOL"];
+            dataGridView4.DataSource = Form3.bs_mol;
+        }
+
+        private void Form7_Load(object sender, EventArgs e)
+        {
+            //
+            // --- [ ЗАГРУЗКА ] ---   ЛИЦА (МОЛ) ----------------------------------------------------
+            Form3.ds.Tables.Add("MOL");
+            load_mol();
+            dataGridView4.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView4.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            textBox_mol_fio.DataBindings.Add(new Binding("Text", Form3.bs_mol, "ФИО", false, DataSourceUpdateMode.Never));
+            textBox_mol_adres.DataBindings.Add(new Binding("Text", Form3.bs_mol, "Адрес", false, DataSourceUpdateMode.Never));
+            textBox_mol_tel.DataBindings.Add(new Binding("Text", Form3.bs_mol, "Телефон", false, DataSourceUpdateMode.Never));
+            // --------------------------------------------------------------------------------------
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            // --- [ ДОБАВЛЕНИЕ ] ---  ЛИЦА
+
+            // проверим все поля на заполненность
+            if (textBox_mol_fio.Text == "" || textBox_mol_adres.Text == "" || textBox_mol_tel.Text == "")
+            {
+                MessageBox.Show("Заполните все поля", "Добавление", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            // запрос на добавление
+            Form3.strSQL = "INSERT INTO mol VALUES (@NAME, @TEL, @AD)";
+
+            // работаем через адаптер и свойство добавления
+            Form3.SQLAdapter.InsertCommand = new SqlCommand(Form3.strSQL, Form3.cn);  // новая команда создана
+            // определим параметры и зададим им значения
+            Form3.SQLAdapter.InsertCommand.Parameters.Add("@NAME", SqlDbType.VarChar).Value = textBox_mol_fio.Text;
+            Form3.SQLAdapter.InsertCommand.Parameters.Add("@TEL", SqlDbType.VarChar).Value = textBox_mol_tel.Text;
+            Form3.SQLAdapter.InsertCommand.Parameters.Add("@AD", SqlDbType.VarChar).Value = textBox_mol_adres.Text;
+            try
+            {
+                Form3.SQLAdapter.InsertCommand.ExecuteNonQuery(); // выполним запрос
+                // если удачно то...
+                load_mol();           // обновим таблицу
+                MessageBox.Show("Успешно добавлен!", "Добавление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                // если не удачно обшика с инфой
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            // --- [ ОБНОВЛЕНИЕ ] ---   ЛИЦА
+
+            if (Form3.ds.Tables["MOL"].Rows.Count > 0)              // проверка на наличие строк в таблице
+            {
+                // проверим все поля на заполненность
+                if (textBox_mol_fio.Text == "" || textBox_mol_adres.Text == "" || textBox_mol_tel.Text == "")
+                {
+                    MessageBox.Show("Заполните все поля", "Обновление", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // запрос на обновление
+                Form3.strSQL = " UPDATE mol SET name = @NAME, tel = @TEL, adres = @AD " +
+                         " WHERE id_mol = @ID_M ";
+
+                Form3.SQLAdapter.UpdateCommand = new SqlCommand(Form3.strSQL, Form3.cn);  // команда для обноления создана
+                // зададим значения параметрам 
+                Form3.SQLAdapter.UpdateCommand.Parameters.Add("@NAME", SqlDbType.VarChar).Value = textBox_mol_fio.Text;
+                Form3.SQLAdapter.UpdateCommand.Parameters.Add("@TEL", SqlDbType.VarChar).Value = textBox_mol_tel.Text;
+                Form3.SQLAdapter.UpdateCommand.Parameters.Add("@AD", SqlDbType.VarChar).Value = textBox_mol_adres.Text;
+                Form3.SQLAdapter.UpdateCommand.Parameters.Add("@ID_M", SqlDbType.Int).Value =
+                    Convert.ToInt32(Form3.ds.Tables["MOL"].Rows[dataGridView4.CurrentRow.Index][0]);
+                try
+                {
+                    Form3.SQLAdapter.UpdateCommand.ExecuteNonQuery(); // выполним запрос
+                    // если удачно то...
+                    load_mol();           // обновим таблицу
+                    MessageBox.Show("Запись успешно обновлена!", "Обновление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    // если запрос выполнился не удачно то ошибка с инфой
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+                MessageBox.Show("Таблица пуста", "Обновление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            // --- [ УДАЛЕНИЕ ВЫБРАННЫХ ] ---   ЛИЦА
+
+            if (Form3.ds.Tables["MOL"].Rows.Count > 0)              // проверка на наличие строк в таблице
+            {
+                Form3.strSQL = " DELETE FROM mol WHERE id_mol = @ID_M ";
+
+                Form3.SQLAdapter.DeleteCommand = new SqlCommand(Form3.strSQL, Form3.cn);
+                // Если нажата кномка да, удаления не избежать.
+                if (DialogResult.Yes == MessageBox.Show("Вы уверены в удалении? \nЗаписей:  "
+                    + dataGridView4.SelectedRows.Count.ToString(), "Удаление", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question, MessageBoxDefaultButton.Button1))
+                {
+                    try
+                    {
+                        foreach (DataGridViewRow drv in dataGridView4.SelectedRows)
+                        {
+                            Form3.SQLAdapter.DeleteCommand.Parameters.Add("@ID_M", SqlDbType.Int).Value =
+                                Convert.ToInt32(Form3.ds.Tables["MOL"].Rows[drv.Index][0]);
+
+                            Form3.SQLAdapter.DeleteCommand.ExecuteNonQuery();
+                            Form3.SQLAdapter.DeleteCommand.Parameters.Clear();
+                        }
+                        load_mol();           // обновим таблицу
+                        MessageBox.Show("Успешно удалено!", "Удаление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            else
+                MessageBox.Show("Таблица пуста", "Удаление", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }

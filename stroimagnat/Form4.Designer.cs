964fed221dcddf19193a6d73e3a66d67c6638e84
electron101 <sabashlive@gmail.com>
2016-12-07 10:56:52 +0300

    Форма для материала готова и все функции тоже.

--- stroimagnat/Form4.Designer.cs
+++ stroimagnat/Form4.Designer.cs
@@ -28,19 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_prod_cena = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_prod_ediz = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_prod_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(657, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(473, 454);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox_prod_cena);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox_prod_ediz);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox_prod_name);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(504, 59);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(263, 163);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Добавление/Обновление";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(153, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Обновить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_prod_cena
+            // 
+            this.textBox_prod_cena.Location = new System.Drawing.Point(117, 71);
+            this.textBox_prod_cena.Name = "textBox_prod_cena";
+            this.textBox_prod_cena.Size = new System.Drawing.Size(136, 20);
+            this.textBox_prod_cena.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Цена за единицу";
+            // 
+            // textBox_prod_ediz
+            // 
+            this.textBox_prod_ediz.Location = new System.Drawing.Point(117, 45);
+            this.textBox_prod_ediz.Name = "textBox_prod_ediz";
+            this.textBox_prod_ediz.Size = new System.Drawing.Size(136, 20);
+            this.textBox_prod_ediz.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Единица измерения";
+            // 
+            // textBox_prod_name
+            // 
+            this.textBox_prod_name.Location = new System.Drawing.Point(117, 19);
+            this.textBox_prod_name.Name = "textBox_prod_name";
+            this.textBox_prod_name.Size = new System.Drawing.Size(136, 20);
+            this.textBox_prod_name.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Наименование";
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.ClientSize = new System.Drawing.Size(792, 573);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form4";
             this.Text = "Материалы";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_prod_cena;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_prod_ediz;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_prod_name;
+        private System.Windows.Forms.Label label1;
     }
 }
--- stroimagnat/Form4.cs
+++ stroimagnat/Form4.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace stroimagnat
 {
@@ -15,6 +16,145 @@ namespace stroimagnat
         public Form4()
         {
             InitializeComponent();
+        }
+
+        static public void load_product()                   // функция для отображения информации о продукте в datagridview
+        {
+            Form3.ds.Tables["PRODUCT"].Clear();
+            Form3.strSQL = "SELECT id_product AS '№_Продукта', name AS 'Наименование', ed_izmer AS 'Единица измерения', cena AS 'Цена' FROM product";
+            Form3.SQLAdapter = new SqlDataAdapter(Form3.strSQL, Form3.cn);
+
+            Form3.SQLAdapter.Fill(Form3.ds, "PRODUCT");
+
+            Form3.bs_product.DataSource = Form3.ds.Tables["PRODUCT"];
+            Program.F4.dataGridView1.DataSource = Form3.bs_product;
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            //
+            // --- [ ЗАГРУЗКА ] ---   ПРОДУКТЫ ------------------------------------------------------
+            Form3.ds.Tables.Add("PRODUCT");
+            load_product();
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            textBox_prod_name.DataBindings.Add(new Binding("Text", Form3.bs_product, "Наименование", false, DataSourceUpdateMode.Never));
+            textBox_prod_ediz.DataBindings.Add(new Binding("Text", Form3.bs_product, "Единица измерения", false, DataSourceUpdateMode.Never));
+            textBox_prod_cena.DataBindings.Add(new Binding("Text", Form3.bs_product, "Цена", false, DataSourceUpdateMode.Never));
+
+            // --------------------------------------------------------------------------------------
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // --- [ ДОБАВЛЕНИЕ ] ---  ПРОДУКТЫ
+
+            // проверим все поля на заполненность
+            if (textBox_prod_name.Text == "" || textBox_prod_ediz.Text == "" || textBox_prod_cena.Text == "")
+            {
+                MessageBox.Show("Заполните все поля", "Добавление", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            // запрос на добавление
+            Form3.strSQL = "INSERT INTO product VALUES (@NAME, @ED, @CENA)";
+
+            // работаем через адаптер и свойство добавления
+            Form3.SQLAdapter.InsertCommand = new SqlCommand(Form3.strSQL, Form3.cn);  // новая команда создана
+            // определим параметры и зададим им значения
+            Form3.SQLAdapter.InsertCommand.Parameters.Add("@NAME", SqlDbType.VarChar).Value = textBox_prod_name.Text;
+            Form3.SQLAdapter.InsertCommand.Parameters.Add("@ED", SqlDbType.VarChar).Value = textBox_prod_ediz.Text;
+            Form3.SQLAdapter.InsertCommand.Parameters.Add("@CENA", SqlDbType.Decimal).Value = textBox_prod_cena.Text;
+            try
+            {
+                Form3.SQLAdapter.InsertCommand.ExecuteNonQuery(); // выполним запрос
+                // если удачно то...
+                load_product();           // обновим таблицу
+                MessageBox.Show("Успешно добавлен!", "Добавление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                // если не удачно обшика с инфой
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // --- [ ОБНОВЛЕНИЕ ] ---   ПРОДУКТЫ
+
+            if (Form3.ds.Tables["PRODUCT"].Rows.Count > 0)              // проверка на наличие строк в таблице
+            {
+                // проверим все поля на заполненность
+                if (textBox_prod_name.Text == "" || textBox_prod_ediz.Text == "" || textBox_prod_cena.Text == "")
+                {
+                    MessageBox.Show("Заполните все поля", "Обновление", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // запрос на обновление
+                Form3.strSQL = " UPDATE product SET name = @NAME, ed_izmer = @ED, cena = @CENA " +
+                         " WHERE id_product = @ID_P ";
+
+                Form3.SQLAdapter.UpdateCommand = new SqlCommand(Form3.strSQL, Form3.cn);  // команда для обноления создана
+                // зададим значения параметрам 
+                Form3.SQLAdapter.UpdateCommand.Parameters.Add("@NAME", SqlDbType.VarChar).Value = textBox_prod_name.Text;
+                Form3.SQLAdapter.UpdateCommand.Parameters.Add("@ED", SqlDbType.VarChar).Value = textBox_prod_ediz.Text;
+                Form3.SQLAdapter.UpdateCommand.Parameters.Add("@CENA", SqlDbType.Decimal).Value = textBox_prod_cena.Text;
+                Form3.SQLAdapter.UpdateCommand.Parameters.Add("@ID_P", SqlDbType.Int).Value =
+                    Convert.ToInt32(Form3.ds.Tables["PRODUCT"].Rows[dataGridView1.CurrentRow.Index][0]);
+                try
+                {
+                    Form3.SQLAdapter.UpdateCommand.ExecuteNonQuery(); // выполним запрос
+                    // если удачно то...
+                    load_product();           // обновим таблицу
+                    MessageBox.Show("Запись успешно обновлена!", "Обновление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    // если запрос выполнился не удачно то ошибка с инфой
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+                MessageBox.Show("Таблица пуста", "Обновление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // --- [ УДАЛЕНИЕ ВЫБРАННЫХ ] ---   ПРОДУКТЫ
+
+            if (Form3.ds.Tables["PRODUCT"].Rows.Count > 0)              // проверка на наличие строк в таблице
+            {
+                Form3.strSQL = " DELETE FROM product WHERE id_product = @ID_P ";
+
+                Form3.SQLAdapter.DeleteCommand = new SqlCommand(Form3.strSQL, Form3.cn);
+                // Если нажата кномка да, удаления не избежать.
+                if (DialogResult.Yes == MessageBox.Show("Вы уверены в удалении? \nЗаписей:  "
+                    + dataGridView1.SelectedRows.Count.ToString(), "Удаление", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question, MessageBoxDefaultButton.Button1))
+                {
+                    try
+                    {
+                        foreach (DataGridViewRow drv in dataGridView1.SelectedRows)
+                        {
+                            Form3.SQLAdapter.DeleteCommand.Parameters.Add("@ID_P", SqlDbType.Int).Value =
+                                Convert.ToInt32(Form3.ds.Tables["PRODUCT"].Rows[drv.Index][0]);
+
+                            Form3.SQLAdapter.DeleteCommand.ExecuteNonQuery();
+                            Form3.SQLAdapter.DeleteCommand.Parameters.Clear();
+                        }
+                        load_product();           // обновим таблицу
+                        MessageBox.Show("Успешно удалено!", "Удаление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            else
+                MessageBox.Show("Таблица пуста", "Удаление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
         }
     }
 }

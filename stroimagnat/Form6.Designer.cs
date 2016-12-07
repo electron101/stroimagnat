047bd9dbfe9c79972c1c2726fe714e1539d7442c
electron101 <sabashlive@gmail.com>
2016-12-07 09:30:59 +0300

    Форма поставщиков сделана. Все кнопки работают

--- stroimagnat/Form6.Designer.cs
+++ stroimagnat/Form6.Designer.cs
@@ -28,19 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox_post_bank = new System.Windows.Forms.TextBox();
+            this.label21 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox_post_tel = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_post_adres = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_post_name = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(650, 241);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Удалить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_post_bank);
+            this.groupBox2.Controls.Add(this.label21);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.textBox_post_tel);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox_post_adres);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBox_post_name);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(498, 50);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(263, 185);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Добавление/Обновление";
+            // 
+            // textBox_post_bank
+            // 
+            this.textBox_post_bank.Location = new System.Drawing.Point(127, 97);
+            this.textBox_post_bank.Name = "textBox_post_bank";
+            this.textBox_post_bank.Size = new System.Drawing.Size(125, 20);
+            this.textBox_post_bank.TabIndex = 10;
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(7, 100);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(93, 13);
+            this.label21.TabIndex = 9;
+            this.label21.Text = "Банковский счёт";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(152, 152);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Обновить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(152, 123);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Добавить";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox_post_tel
+            // 
+            this.textBox_post_tel.Location = new System.Drawing.Point(127, 71);
+            this.textBox_post_tel.Name = "textBox_post_tel";
+            this.textBox_post_tel.Size = new System.Drawing.Size(125, 20);
+            this.textBox_post_tel.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Телефон";
+            // 
+            // textBox_post_adres
+            // 
+            this.textBox_post_adres.Location = new System.Drawing.Point(127, 45);
+            this.textBox_post_adres.Name = "textBox_post_adres";
+            this.textBox_post_adres.Size = new System.Drawing.Size(125, 20);
+            this.textBox_post_adres.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Адрес";
+            // 
+            // textBox_post_name
+            // 
+            this.textBox_post_name.Location = new System.Drawing.Point(127, 19);
+            this.textBox_post_name.Name = "textBox_post_name";
+            this.textBox_post_name.Size = new System.Drawing.Size(125, 20);
+            this.textBox_post_name.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Наименование";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 35);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(445, 454);
+            this.dataGridView2.TabIndex = 15;
             // 
             // Form6
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.ClientSize = new System.Drawing.Size(792, 573);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.dataGridView2);
             this.Name = "Form6";
             this.Text = "Поставщики";
+            this.Load += new System.EventHandler(this.Form6_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox_post_bank;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox_post_tel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_post_adres;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_post_name;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
--- stroimagnat/Form6.cs
+++ stroimagnat/Form6.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace stroimagnat
 {
@@ -15,6 +16,151 @@ namespace stroimagnat
         public Form6()
         {
             InitializeComponent();
+        }
+
+        void load_post()                   // функция для отображения информации
+        {
+            Form3.ds.Tables["POST"].Clear();
+            Form3.strSQL = " SELECT id_post AS '№_Поставщика', name AS 'Наименование', " + 
+                           " adres AS 'Адрес', tel AS 'Телефон', bank_schet AS " + 
+                           " 'Банковский счёт' FROM postav ";
+            Form3.SQLAdapter = new SqlDataAdapter(Form3.strSQL, Form3.cn);
+
+            Form3.SQLAdapter.Fill(Form3.ds, "POST");
+
+            Form3.bs_post.DataSource = Form3.ds.Tables["POST"];
+            dataGridView2.DataSource = Form3.bs_post;
+        }
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+            //
+            // --- [ ЗАГРУЗКА ] ---   ПОСТАВЩИКИ ----------------------------------------------------
+            Form3.ds.Tables.Add("POST");
+            load_post();
+            dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            textBox_post_name.DataBindings.Add(new Binding("Text", Form3.bs_post, "Наименование", false, DataSourceUpdateMode.Never));
+            textBox_post_adres.DataBindings.Add(new Binding("Text", Form3.bs_post, "Адрес", false, DataSourceUpdateMode.Never));
+            textBox_post_tel.DataBindings.Add(new Binding("Text", Form3.bs_post, "Телефон", false, DataSourceUpdateMode.Never));
+            textBox_post_bank.DataBindings.Add(new Binding("Text", Form3.bs_post, "Банковский счёт", false, DataSourceUpdateMode.Never));
+            // --------------------------------------------------------------------------------------
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // --- [ ДОБАВЛЕНИЕ ] ---  ПОСТАВЩИКИ
+
+            // проверим все поля на заполненность
+            if (textBox_post_name.Text == "" || textBox_post_adres.Text == ""
+                || textBox_post_tel.Text == "" || textBox_post_bank.Text == "")
+            {
+                MessageBox.Show("Заполните все поля", "Добавление", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            // запрос на добавление
+            Form3.strSQL = "INSERT INTO postav VALUES (@NAME, @AD, @TEL, @BANK)";
+
+            // работаем через адаптер и свойство добавления
+            Form3.SQLAdapter.InsertCommand = new SqlCommand(Form3.strSQL, Form3.cn);  // новая команда создана
+            // определим параметры и зададим им значения
+            Form3.SQLAdapter.InsertCommand.Parameters.Add("@NAME", SqlDbType.VarChar).Value = textBox_post_name.Text;
+            Form3.SQLAdapter.InsertCommand.Parameters.Add("@AD", SqlDbType.VarChar).Value = textBox_post_adres.Text;
+            Form3.SQLAdapter.InsertCommand.Parameters.Add("@TEL", SqlDbType.VarChar).Value = textBox_post_tel.Text;
+            Form3.SQLAdapter.InsertCommand.Parameters.Add("@BANK", SqlDbType.VarChar).Value = textBox_post_bank.Text;
+            try
+            {
+                Form3.SQLAdapter.InsertCommand.ExecuteNonQuery(); // выполним запрос
+                // если удачно то...
+                load_post();           // обновим таблицу
+                MessageBox.Show("Успешно добавлен!", "Добавление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                // если не удачно обшика с инфой
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // --- [ ОБНОВЛЕНИЕ ] ---   ПОСТАВЩИКИ
+
+            if (Form3.ds.Tables["POST"].Rows.Count > 0)              // проверка на наличие строк в таблице
+            {
+                // проверим все поля на заполненность
+                if (textBox_post_name.Text == "" || textBox_post_adres.Text == ""
+                    || textBox_post_tel.Text == "" || textBox_post_bank.Text == "")
+                {
+                    MessageBox.Show("Заполните все поля", "Обновление", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // запрос на обновление
+                Form3.strSQL = " UPDATE postav SET name = @NAME, adres = @AD, tel = @TEL, bank_schet = @BANK " +
+                         " WHERE id_post = @ID_P ";
+
+                Form3.SQLAdapter.UpdateCommand = new SqlCommand(Form3.strSQL, Form3.cn);  // команда для обноления создана
+                // зададим значения параметрам 
+                Form3.SQLAdapter.UpdateCommand.Parameters.Add("@NAME", SqlDbType.VarChar).Value = textBox_post_name.Text;
+                Form3.SQLAdapter.UpdateCommand.Parameters.Add("@AD", SqlDbType.VarChar).Value = textBox_post_adres.Text;
+                Form3.SQLAdapter.UpdateCommand.Parameters.Add("@TEL", SqlDbType.VarChar).Value = textBox_post_tel.Text;
+                Form3.SQLAdapter.UpdateCommand.Parameters.Add("@BANK", SqlDbType.VarChar).Value = textBox_post_bank.Text;
+                Form3.SQLAdapter.UpdateCommand.Parameters.Add("@ID_P", SqlDbType.Int).Value =
+                    Convert.ToInt32(Form3.ds.Tables["POST"].Rows[dataGridView2.CurrentRow.Index][0]);
+                try
+                {
+                    Form3.SQLAdapter.UpdateCommand.ExecuteNonQuery(); // выполним запрос
+                    // если удачно то...
+                    load_post();           // обновим таблицу
+                    MessageBox.Show("Запись успешно обновлена!", "Обновление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    // если запрос выполнился не удачно то ошибка с инфой
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+                MessageBox.Show("Таблица пуста", "Обновление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // --- [ УДАЛЕНИЕ ВЫБРАННЫХ ] ---   ПОСТАВЩИКИ
+
+            if (Form3.ds.Tables["POST"].Rows.Count > 0)              // проверка на наличие строк в таблице
+            {
+                Form3.strSQL = " DELETE FROM postav WHERE id_post = @ID_P ";
+
+                Form3.SQLAdapter.DeleteCommand = new SqlCommand(Form3.strSQL, Form3.cn);
+                // Если нажата кномка да, удаления не избежать.
+                if (DialogResult.Yes == MessageBox.Show("Вы уверены в удалении? \nЗаписей:  "
+                    + dataGridView2.SelectedRows.Count.ToString(), "Удаление", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question, MessageBoxDefaultButton.Button1))
+                {
+                    try
+                    {
+                        foreach (DataGridViewRow drv in dataGridView2.SelectedRows)
+                        {
+                            Form3.SQLAdapter.DeleteCommand.Parameters.Add("@ID_P", SqlDbType.Int).Value =
+                                Convert.ToInt32(Form3.ds.Tables["POST"].Rows[drv.Index][0]);
+
+                            Form3.SQLAdapter.DeleteCommand.ExecuteNonQuery();
+                            Form3.SQLAdapter.DeleteCommand.Parameters.Clear();
+                        }
+                        load_post();           // обновим таблицу
+                        MessageBox.Show("Успешно удалено!", "Удаление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            else
+                MessageBox.Show("Таблица пуста", "Удаление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
         }
     }
 }
